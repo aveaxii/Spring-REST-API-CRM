5fbe5c87ddb47750dba7bb7ede4a2d2f8351fa62
Kim Alex <iamnotxirere@gmail.com>
2024-02-13 21:45:16 +0900

    task 3.1.4 v2.2
    - Problem with registration

---
+++ src/main/java/task312/demo/services/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package task312.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import task312.demo.models.User;
+import task312.demo.repositories.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return user.get();
+    }
+}
